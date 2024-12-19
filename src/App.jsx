068b93708aa9461e75bf25668797f735c1a8b7c2
fisheeesh <swanphyo444@gmail.com>
2024-12-19 21:34:57 +0700

    design with mui components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useContext, useState } from "react";
-import Item from "./Item";
-import List from "./List";
-import Form from "./Form";
-import { AppContext } from "./ThemedApp";
+import { useState } from "react";
+
+import { Box, Container } from "@mui/material";
+
+import Item from "./components/Item";
+import Form from "./components/Form";
+import { useApp } from "./ThemedApp";
+import Header from "./components/Header";
 
 export default function App() {
-  const { mode, setMode } = useContext(AppContext);
+  const { showForm } = useApp();
 
-  const [showForm, setShowForm] = useState(false);
   const [data, setData] = useState([
-    { id: 1, content: "Hello, World!", name: "Alice" },
-    { id: 2, content: "React is fun.", name: "Bob" },
     { id: 3, content: "Yay, interesting.", name: "Chris" },
+    { id: 2, content: "React is fun.", name: "Bob" },
+    { id: 1, content: "Hello, World!", name: "Alice" },
   ]);
 
   const remove = (id) => {
@@ -20,75 +22,22 @@ export default function App() {
 
   const add = (content, name) => {
     const id = data[data.length - 1].id + 1;
-    setData((prevData) => [...prevData, { id, content, name }]);
+    setData((prevData) => [{ id, content, name }, ...prevData,]);
   };
 
-  return (
-    <div
-      style={{
-        minHeight: 1500,
-        background: mode === "dark" ? "black" : "white",
-        color: mode === "dark" ? "white" : "black",
-        paddingTop: 20,
-      }}
-    >
-      <div style={{ maxWidth: 600, margin: "20px auto" }}>
-        <h1
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            marginBottom: 15,
-          }}
-        >
-          Yaycha
-          <div>
-            <button
-              onClick={() => setShowForm(!showForm)}
-              style={{
-                width: 32,
-                height: 32,
-                borderRadius: 50,
-                border: "0 none",
-                background: showForm ? "#dc3545" : "#0d6efd",
-                color: "white",
-                cursor: "pointer",
-              }}
-            >
-              {showForm ? "x" : "+"}
-            </button>
+  console.log(data)
 
-            <button
-              onClick={() =>
-                setMode((prevState) =>
-                  prevState === "dark" ? "light" : "dark"
-                )
-              }
-              style={{
-                marginLeft: 8,
-                padding: "0 20px",
-                height: 32,
-                borderRadius: 32,
-                border: "0 none",
-                background: mode === "dark" ? "#333" : "#ddd",
-                color: mode === "dark" ? "white" : "dark",
-              }}
-            >
-              {mode === "dark" ? "Light" : "Dark"}
-            </button>
-          </div>
-        </h1>
+  return (
+    <Box>
+      <Header />
+      <Container
+        maxWidth="sm"
+        sx={{ mt: 4 }}
+      >
         {showForm && <Form add={add} />}
-        <List>
-          {!!data.length &&
-            data.map((item) => (
-              <Item key={item.id} item={item} remove={remove} />
-            ))}
-          {!data.length && (
-            <p style={{ textAlign: "center", margin: "20px 0" }}>No Item(s)</p>
-          )}
-        </List>
-      </div>
-    </div>
+
+        {data.map(item => (<Item key={item.id} item={item} remove={remove} />))}
+      </Container>
+    </Box>
   );
 }
--- src/ThemedApp.jsx
+++ src/ThemedApp.jsx
@@ -1,15 +1,31 @@
 /* eslint-disable react-refresh/only-export-components */
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
+
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
 import App from "./App";
 
-export const AppContext = createContext();
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+const AppContext = createContext();
+
+export function useApp() {
+  return useContext(AppContext);
+}
 
 export default function ThemeApp() {
-  const [mode, setMode] = useState("dark");
+  const [showForm, setShowForm] = useState(false);
+
   return (
-    <AppContext.Provider value={{ mode, setMode }}>
-      <App />
-    </AppContext.Provider>
+    <ThemeProvider theme={theme}>
+      <AppContext.Provider value={{ showForm, setShowForm }}>
+        <App />
+        <CssBaseline />
+      </AppContext.Provider>
+    </ThemeProvider>
   );
 }
